08197581badbc1d24e701e8484645cbea3a7ac09
Jay Joshi <joshi.jay.d@gmail.com>
2016-04-09 18:10:35 +0530

    hibernate.hbm2ddl.auto set to create

--- src/main/java/com/platovi/model/City.java
+++ src/main/java/com/platovi/model/City.java
@@ -1,17 +1,10 @@
 package com.platovi.model;
 
-import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.List;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
 
 @Entity
