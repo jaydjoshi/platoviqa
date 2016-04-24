9b6caaaa585b35d4a541dcf91050c883060df5b1
Jay Joshi <joshi.jay.d@gmail.com>
2016-04-24 13:05:07 +0530

    s

--- src/main/webapp/app/controllers/platoviController.js
+++ src/main/webapp/app/controllers/platoviController.js
@@ -905,7 +905,7 @@ app.controller('resultDetailController',function($scope,$rootScope,$timeout,city
 				        window.google.maps.event.trigger(map, 'resize');
 				        map.setCenter(new google.maps.LatLng($scope.currentCity.latitude,$scope.currentCity.longitude));
 				      
-				      }, 2000)
+				      }, 4000)
 			});
 		
     	
