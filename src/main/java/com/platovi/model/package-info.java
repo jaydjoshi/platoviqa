5cad56af4b25b040f751b0f49db69506a938a5d4
Jay Joshi <joshi.jay.d@gmail.com>
2016-05-28 22:44:49 +0530

    sitemap fixes

---
+++ src/main/java/com/platovi/model/package-info.java
@@ -0,0 +1,9 @@
+@XmlSchema(
+    namespace = "http://www.sitemaps.org/schemas/sitemap/0.9",
+    elementFormDefault = XmlNsForm.QUALIFIED)
+
+package com.platovi.model;
+
+import javax.xml.bind.annotation.XmlNsForm;
+import javax.xml.bind.annotation.XmlSchema;
+
